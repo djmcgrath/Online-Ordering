8d825d5d27cb63bc1e92402cc8d611f6e44e44bb
Taylor Riley <t_riley13@icloud.com>
2023-11-28 14:01:38 -0700

    stashing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -33,6 +33,33 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <div className="collapse collapse-plus bg-base-200 my-2">
+                <input type="radio" name="my-accordion-3" checked="checked" />
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200 my-2">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200 my-2">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
         </div>
 
     )
